3c1b0ff43d6bf91da097977ff744a4a3b3ca5b02
kic35 <keiichisato0305@gmail.com>
2017-01-12 14:06:21 +0900

    1/12時点

--- osakana1.js
+++ osakana1.js
@@ -1,35 +1,176 @@
-// 地図の中心を決める
-var latlng = new google.maps.LatLng(35.387951, 139.427123);
- 
+//非対応ブラウザへのアラート
+if (!navigator.geolocation) {
+    alert('お使いのブラウザはサポート対象外です。別のブラウザでお試しください。');
+}
+
 // 地図を表示するためのオプションを設定する
 var opt = {
-  zoom: 17,
-  center: latlng,
-  mapTypeId: google.maps.MapTypeId.ROADMAP
+    zoom: 17,
+    center: new google.maps.LatLng(35.387951, 139.427123),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
 };
  
 // 地図を表示する。
 var map = new google.maps.Map(document.getElementById('map'), opt);
 
-//指定した画像のアイコンを設置する
-
-
-var image = ['fishicon1.png', 'fishicon2.png', 'fishicon3.png', 'fishicon4.png'];
-var coordinate = [
-    ['鴨池', 35.387115, 139.427281],
-    ['κ', 35.387704, 139.426141],
-    ['θ', 35.388918, 139.427515],
-    ['メディア', 35.388211, 139.427310]
+//アイコンを設置する
+var placedata = [
+    ['鴨池', 35.387115, 139.427281, 'fishicon1.png'],
+    ['κ', 35.387704, 139.426141, 'fishicon2.png'],
+    ['θ', 35.388918, 139.427515, 'fishicon3.png'],
+    ['メディア', 35.388211, 139.427310, 'fishicon4.png'],
+    ['テスト用けーいち自宅', 35.679317, 139.589668, 'fishicon1.png']
 ];
-var marker1, marker2, marker3, marker4;
-var marker = [marker1, marker2, marker3, marker4];
+var marker1, marker2, marker3, marker4, marker5;
+var marker = [marker1, marker2, marker3, marker4, marker5];
 
 var i;
-for (i = 1; i < 5; i++) {
+for (i = 0; i < 5; i++) {
     
-    marker[i - 1] = new google.maps.Marker({
-        position: {lat: coordinate[i - 1][1], lng: coordinate[i - 1][2] },
+    marker[i] = new google.maps.Marker({
+        position: {lat: placedata[i][1], lng: placedata[i][2] },
         map: map,
-        icon: image[i - 1]
+        icon: placedata[i][3]
     });
 }
+
+
+function movetomyhouse() {
+    map.panTo(new google.maps.LatLng(35.679317, 139.589668));
+}
+
+
+
+
+
+
+
+//現在地を追跡,https://syncer.jp/how-to-use-geolocation-api    
+var watchId = navigator.geolocation.watchPosition(
+    function (result) {
+    //    neko.setMap(null); //ここ不安
+        //現在地の取得成功
+        var position = result.coords,
+            radius = position.accuracy,
+            latLng = new google.maps.LatLng(position.latitude, position.longitude),
+
+            neko = new google.maps.Marker({
+                position: latLng,
+                map: map,
+                icon: 'neko.png'
+            });
+       //チェック用、あとで消す 
+        var nichiji = new Date(),
+            year = nichiji.getFullYear(),
+            month = nichiji.getMonth() + 1,
+            week = nichiji.getDay(),
+            day = nichiji.getDate(),
+            hour = nichiji.getHours(),
+            minute = nichiji.getMinutes(),
+            second = nichiji.getSeconds();
+        console.log(year + '/' + month + '/' + day + ' ' + hour + ':' + minute + ':' + second);
+        console.log(position);
+    },
+    //取得に失敗した場合
+    function (error) {
+        // エラーコード(error.code)の番号
+        // 0:UNKNOWN_ERROR				原因不明のエラー
+        // 1:PERMISSION_DENIED			利用者が位置情報の取得を許可しなかった
+        // 2:POSITION_UNAVAILABLE		電波状況などで位置情報が取得できなかった
+        // 3:TIMEOUT					位置情報の取得に時間がかかり過ぎた…
+
+        // エラー番号に対応したメッセージ
+        var errorInfo = [
+            "原因不明のエラーが発生しました…。",
+            "位置情報の取得が許可されませんでした…。",
+            "電波状況などで位置情報が取得できませんでした…。",
+            "位置情報の取得に時間がかかり過ぎてタイムアウトしました…。"
+        ],
+            // エラー番号
+            errorNo = error.code,
+            // エラーメッセージ
+            errorMessage = "[エラー番号: " + errorNo + "]\n" + errorInfo[errorNo];
+
+        // アラート表示
+        alert(errorMessage);
+    },
+    {
+        "enableHighAccuracy": true,
+        "timeout": 8000,
+        "maximumAge": 0
+    }
+);
+    
+
+
+
+/*
+function catchfish() {
+// 現在地を取得
+	navigator.geolocation.getCurrentPosition(
+        //取得に成功した場合
+		function (position) {
+                // 取得したデータの整理
+			var data = position.coords,
+                // データの整理
+                lat = data.latitude,
+                lng = data.longitude,
+                accLatlng = data.accuracy,
+                // 位置情報
+                latlng = new google.maps.LatLng(lat, lng);
+            decision();
+		},
+
+		//取得に失敗した場合
+		function (error) {
+			// エラーコード(error.code)の番号
+			// 0:UNKNOWN_ERROR				原因不明のエラー
+			// 1:PERMISSION_DENIED			利用者が位置情報の取得を許可しなかった
+			// 2:POSITION_UNAVAILABLE		電波状況などで位置情報が取得できなかった
+			// 3:TIMEOUT					位置情報の取得に時間がかかり過ぎた…
+
+			// エラー番号に対応したメッセージ
+			var errorInfo = [
+				"原因不明のエラーが発生しました…。",
+				"位置情報の取得が許可されませんでした…。",
+				"電波状況などで位置情報が取得できませんでした…。",
+				"位置情報の取得に時間がかかり過ぎてタイムアウトしました…。"
+			],
+                // エラー番号
+                errorNo = error.code,
+                // エラーメッセージ
+                errorMessage = "[エラー番号: " + errorNo + "]\n" + errorInfo[errorNo];
+
+			// アラート表示
+			alert(errorMessage);
+		},
+
+		// [第3引数] オプション
+		{
+			"enableHighAccuracy": true,
+			"timeout": 5000,
+			"maximumAge": 2000
+		}
+
+	);
+}
+*/
+
+var counter = 0;
+
+/*
+function decision() {
+    //捕まえられる場合(設定した範囲内に魚がいる場合)
+    if (){
+    //捕獲数+1
+    
+    //アイコン消す
+    marker[i].setMap(null);
+    //つかまえたアラート
+    alert(placedata[][0] + 'の' + placedata[][3] + 'をつかまえた！');
+    }else{
+//無理な場合
+    alert('つかまえられなかったよ！もう少し近づいてね！');
+    }
+}
+*/
